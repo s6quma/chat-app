91b5391c480cb3b856cb0bd6e6c8660b74a405d3
s6quma <s6quma@yahoo.co.jp>
2021-01-18 10:59:47 +0900

    messagesテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :messages, only: [:index, :new, :create]
 end
