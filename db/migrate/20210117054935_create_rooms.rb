66c907c6da0726fc92e75017a503f56834d74e2b
s6quma <s6quma@yahoo.co.jp>
2021-03-01 14:00:48 +0900

    roomDBの変更

--- db/migrate/20210117054935_create_rooms.rb
+++ db/migrate/20210117054935_create_rooms.rb
@@ -1,8 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string  :name,        null: false
-      t.integer  :manager,     null: false
+      t.string :name,         null: false
+      t.integer :manager,     null: false
       t.float :price,         null: false
       t.integer :currency_id, null: false
       t.timestamps
